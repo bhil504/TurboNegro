3f0cba865c3d4fa5874f8550c4d797da93d65328
BhillionDollar <bhilliondollarproductions@gmail.com>
2025-01-19 22:30:48 -0600

    boss fight

--- bossFight.js
+++ bossFight.js
@@ -7,8 +7,12 @@ export default class BossFight extends Phaser.Scene {
         // Load assets
         this.load.image('finalFightBackground', 'assets/Levels/BackGrounds/finalFight.webp');
         this.load.image('beignetBoss', 'assets/Characters/Enemies/Beignet_Boss.png');
-        this.load.image('turboNegroStanding', 'assets/Characters/Character1/TurboNegroStanding/TurboNegroStanding1.png');
-        this.load.image('ground', 'assets/Levels/Platforms/ground.png'); // Placeholder for ground
+        this.load.image('turboNegroStanding1', 'assets/Characters/Character1/TurboNegroStanding/TurboNegroStanding1.png');
+        this.load.image('turboNegroStanding2', 'assets/Characters/Character1/TurboNegroStanding/TurboNegroStanding2.png');
+        this.load.image('turboNegroStanding3', 'assets/Characters/Character1/TurboNegroStanding/TurboNegroStanding3.png');
+        this.load.image('turboNegroStanding4', 'assets/Characters/Character1/TurboNegroStanding/TurboNegroStanding4.png');
+        this.load.image('turboNegroWalking', 'assets/Characters/Character1/TurboNegroWalking.png');
+        this.load.image('ground', 'assets/Levels/Platforms/ground.png');
     }
 
     create() {
@@ -19,14 +23,10 @@ export default class BossFight extends Phaser.Scene {
 
         // Create ground
         this.ground = this.physics.add.staticGroup();
-        this.ground
-            .create(width / 2, height - 50, 'ground')
-            .setDisplaySize(width, 100)
-            .setOrigin(0.5, 0)
-            .refreshBody();
+        this.ground.create(width / 2, height - 50, 'ground').setDisplaySize(width, 100).refreshBody();
 
         // Add player character
-        this.player = this.physics.add.sprite(100, height - 150, 'turboNegroStanding');
+        this.player = this.physics.add.sprite(100, height - 150, 'turboNegroStanding1');
         this.player.setCollideWorldBounds(true);
 
         // Add Beignet Boss
@@ -37,7 +37,21 @@ export default class BossFight extends Phaser.Scene {
         this.physics.add.collider(this.player, this.ground);
         this.physics.add.collider(this.boss, this.ground);
 
-        // Player controls
+        // Player animations
+        this.anims.create({
+            key: 'idle',
+            frames: [
+                { key: 'turboNegroStanding1' },
+                { key: 'turboNegroStanding2' },
+                { key: 'turboNegroStanding3' },
+                { key: 'turboNegroStanding4' },
+            ],
+            frameRate: 4,
+            repeat: -1,
+        });
+        this.anims.create({ key: 'walk', frames: [{ key: 'turboNegroWalking' }], frameRate: 8, repeat: -1 });
+
+        // Player input setup
         this.cursors = this.input.keyboard.createCursorKeys();
 
         // Boss movement setup
@@ -48,10 +62,21 @@ export default class BossFight extends Phaser.Scene {
     update() {
         // Player movement
         this.player.setVelocityX(0);
+
         if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-200);
+            this.player.setVelocityX(-165);
+            this.player.setFlipX(true);
+            this.player.play('walk', true);
         } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(200);
+            this.player.setVelocityX(165);
+            this.player.setFlipX(false);
+            this.player.play('walk', true);
+        } else if (this.player.body.touching.down) {
+            this.player.play('idle', true);
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-500);
         }
 
         // Beignet Boss movement
