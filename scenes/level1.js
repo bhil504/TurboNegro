47813272b440102e3a4e6e4c1d3d8ebb9dbba742
BhillionDollar <bhilliondollarproductions@gmail.com>
2025-01-28 03:54:43 -0600

    modularizing

--- scenes/level1.js
+++ scenes/level1.js
@@ -1,5 +1,5 @@
-import { addFullscreenButton } from '../utils/fullScreenUtils.js';
-import { setupMobileControls } from '../utils/mobileControls.js';
+import { addFullscreenButton } from './utils/fullScreenUtils.js';
+import { setupMobileControls } from './utils/mobileControls.js';
 
 
 
