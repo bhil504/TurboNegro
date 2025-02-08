6d120b8d0e2326d8c27b27469f647745a6df2153
BhillionDollar <bhilliondollarproductions@gmail.com>
2025-02-08 11:39:52 -0600

    adjusting landscape fullscreen

--- utils/fullScreenUtils.js
+++ utils/fullScreenUtils.js
@@ -93,8 +93,8 @@ function adjustScreenForLandscapeFullscreen() {
         fullscreenElement.style.top = "0";
         fullscreenElement.style.left = "50%";
         fullscreenElement.style.transform = "translateX(-50%)";
-        fullscreenElement.style.width = "125vw";
-        fullscreenElement.style.height = "125vh";
+        fullscreenElement.style.width = "200vw";
+        fullscreenElement.style.height = "200vh";
         fullscreenElement.style.display = "flex";
         fullscreenElement.style.justifyContent = "center";
         fullscreenElement.style.alignItems = "center";
